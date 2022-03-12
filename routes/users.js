aebcacd67f911b7e733c2c218d6bd920693e28c6
cheriyankmaman <cheriyankmaman@gmail.com>
2022-03-12 12:40:18 +0000

    mongo db implemented for user api GET and POST

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,40 @@
 var express = require('express');
+const { default: mongoose } = require('mongoose');
+var mongo = require('mongoose');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+/* connect to mongo db */
+mongo.connect("mongodb+srv://cheriyan:kadachira@cluster1.5come.mongodb.net/expressapp?retryWrites=true&w=majority", function(error){
+  if(!error){
+    console.log("db connected! :-)");
+  } else{
+    console.log("db connection error!");
+  }
 });
 
-router.post('/', function(req, res, next) {
+var schema = new mongoose.Schema({
+  name: String,
+  id: Number,
+  role: String,
+  username: String
+});
+var document = mongoose.model("users", schema);
+
+/* GET users listing. */
+router.get('/', async function(req, res, next) {
+  
+  var a = await document.find({});
+  console.log(a);
+
+  res.header('Content-Type', 'application/json');
+  res.send(a);
+});
+
+router.post('/', async function(req, res, next) {
+
+  await document.insertMany([req.body]);
+
   res.header('Content-Type', 'application/json');
   res.send(req.body);
 });
